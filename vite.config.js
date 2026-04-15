53537e883ac8c4792c7b6a01df5e21a337b6cb5e
cyn <letcindyknow@gmail.com>
2026-04-15 00:33:46 -0400

    fixed bugs for vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Change "/peek-inside-my-brain/" to match your GitHub repo name
 export default defineConfig({
   plugins: [react()],
   base: "/mind/",
