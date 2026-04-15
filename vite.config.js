25b9bbfcd2cb28010d9e3ef2f7a0af7bba8a1a69
cyn <letcindyknow@gmail.com>
2026-04-15 00:26:58 -0400

    gh pages addition

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Change "/peek-inside-my-brain/" to match your GitHub repo name
 export default defineConfig({
   plugins: [react()],
+  base: "/mind/",
 });
